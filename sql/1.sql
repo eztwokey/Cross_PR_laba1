b848128a4581cc0d55ae314725a818a944f98a17
Кирилл <49287393+eztwokey@users.noreply.github.com>
2021-10-19 22:42:20 +0300

    Add files via upload

---
+++ sql/1.sql
@@ -0,0 +1,12 @@
+|              Title              |          Genre          |
+|---------------------------------|-------------------------|
+| Enter the Anime                 | Documentary             |
+| Dark Forces                     | Thriller                |
+| The App                         | Science fiction/Drama   |
+| The Open House                  | Horror thriller         |
+| Kaali Khuhi                     | Mystery                 |
+| Drive                           | Action                  |
+| Leyla Everlasting               | Comedy                  |
+| The Last Days of American Crime | Heist film/Thriller     |
+| Paradox                         | Musical/Western/Fantasy |
+| Sardar Ka Grandson              | Comedy                  |
---
+++ sql/2.sql
@@ -0,0 +1,7 @@
+|               Title               |    Genre    |
+|-----------------------------------|-------------|
+| Enter the Anime                   | Documentary |
+| Searching for Sheela              | Documentary |
+| After the Raid                    | Documentary |
+| Hello Privilege. It's Me, Chelsea | Documentary |
+| After Maria                       | Documentary |
