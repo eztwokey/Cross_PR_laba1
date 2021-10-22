52bacf408c64d09f3a3867ea87d876c5288fd811
Кирилл <49287393+eztwokey@users.noreply.github.com>
2021-10-22 12:58:32 +0300

    Update 1.sql

--- sql/1.sql
+++ sql/1.sql
@@ -1,12 +1,2 @@
-|              Title              |          Genre          |
-|---------------------------------|-------------------------|
-| Enter the Anime                 | Documentary             |
-| Dark Forces                     | Thriller                |
-| The App                         | Science fiction/Drama   |
-| The Open House                  | Horror thriller         |
-| Kaali Khuhi                     | Mystery                 |
-| Drive                           | Action                  |
-| Leyla Everlasting               | Comedy                  |
-| The Last Days of American Crime | Heist film/Thriller     |
-| Paradox                         | Musical/Western/Fantasy |
-| Sardar Ka Grandson              | Comedy                  |
+select title, genre 
+from netflix limit 10;
